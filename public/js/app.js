17a6812ddf348c351e9430e0da2199caa5562130
Alan K <akillian@outlook.com>
2018-06-22 13:48:20 -0400

    commit

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,8 @@
 window.addEventListener("load", () => {
   const el = $("#app");
 
+  // fixer api key- 0c21fcce6b2939f9b24813ddbe7de7e1
+
   // Compile Handlebar Templates
   // Note: Handlebars.compile() method returns a function.
   const errorTemplate = Handlebars.compile($("#error-template").html());
