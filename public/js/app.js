f1fd2b12779846111782f0ef12ce425114d093a4
Alan K <akillian@outlook.com>
2018-06-23 17:56:49 -0400

    commit

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,12 @@
-window.addEventListener("load", () => {
-  const el = $("#app");
-
-  // fixer api key- 0c21fcce6b2939f9b24813ddbe7de7e1
+window.addEventListener('load', () => {
+  const el = $('#app');
 
   // Compile Handlebar Templates
-  // Note: Handlebars.compile() method returns a function.
-  const errorTemplate = Handlebars.compile($("#error-template").html());
-  const ratesTemplate = Handlebars.compile($("#rates-template").html());
-  const exchangeTemplate = Handlebars.compile($("#exchange-template").html());
-  const historicalTemplate = Handlebars.compile($("#historical-template").html);
+  const errorTemplate = Handlebars.compile($('#error-template').html());
+  const ratesTemplate = Handlebars.compile($('#rates-template').html());
+  const exchangeTemplate = Handlebars.compile($('#exchange-template').html());
+  const historicalTemplate = Handlebars.compile($('#historical-template').html());
 
   const html = ratesTemplate();
-  el.html = html(html);
-}); // End window.addEventListener
+  el.html(html);
+});
