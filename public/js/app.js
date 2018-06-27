c9304e873af4fd31d02c0d5ddd9dba6f776e3ef6
akillian@outlook.com <akillian@outlook.com>
2018-06-27 18:27:12 -0400

    Add API's

--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,26 @@
 window.addEventListener("load", () => {
   const el = $("#app");
 
+  // =========================================================== //
   // Compile Handlebar Templates
   // Note: Handlebars.compile returns a function. i.e. to use
   // ratesTemplate would go "let html = ratesTemplate()" and variable
   // html will now be equal to the html in template plus any variables
   // with substituted values
+  // =========================================================== //
   const errorTemplate = Handlebars.compile($("#error-template").html());
   const ratesTemplate = Handlebars.compile($("#rates-template").html());
   const exchangeTemplate = Handlebars.compile($("#exchange-template").html());
   const historicalTemplate = Handlebars.compile(
     $("#historical-template").html()
   );
+  // =========================================================== //
+  // ========== End Handlebar Templates Creation =============== //
+  // =========================================================== //
 
-  // Router Declaration
+  // =========================================================== //
+  // Front-End Router Declaration via the vanilla-router package //
+  // =========================================================== //
   const router = new Router({
     mode: "history",
     page404: path => {
@@ -25,11 +32,47 @@ window.addEventListener("load", () => {
       el.html(html);
     }
   });
+  // =========================================================== //
+  // ========== End Front-End Vanilla-Router Declaration ======= //
+  // =========================================================== //
 
-  router.add("/", () => {
+  // =========================================================== //
+  // ========= Front-End vanilla-router Routes ================= //
+  // =========================================================== //
+  // Instantiate api handler
+  const api = axios.create({
+    baseURL: "http://localhost:3000/api",
+    timeout: 5000
+  });
+
+  // Display Error Banner
+  const showError = error => {
+    const { title, message } = error.response.data;
+    const html = errorTemplate({ color: "red", title, message });
+    el.html(html);
+  };
+
+  // Display Latest Currency Rates
+  /* jshint ignore:start */
+  router.add("/", async () => {
+    // Display loader first
     let html = ratesTemplate();
     el.html(html);
+    try {
+      // Load Currency Rates
+      const response = await api.get("/rates");
+      const { base, date, rates } = response.data;
+      // Display Rates Table
+      html = ratesTemplate({ base, date, rates });
+      el.html(html);
+    } catch (error) {
+      showError(error);
+    } finally {
+      // Remove loader status
+      $(".loading").removeClass("loading");
+    }
   });
+  /* jshint ignore:end */
 
   router.add("/exchange", () => {
     let html = exchangeTemplate();
@@ -40,6 +83,9 @@ window.addEventListener("load", () => {
     let html = historicalTemplate();
     el.html(html);
   });
+  // =========================================================== //
+  // ================ End vanilla-router Front-End Routes ====== //
+  // =========================================================== //
 
   // Navigate app to current url
   // This is at heart of how front-end routing works
@@ -61,7 +107,7 @@ window.addEventListener("load", () => {
     // Navigate to clicked url
     const href = target.attr("href");
     const path = href.substr(href.lastIndexOf("/"));
-    
+
     // This is what makes app navigate to
     // a new uri everytime menu item is clicked
     router.navigateTo(path);
--- server.js
+++ server.js
@@ -1,24 +1,68 @@
-require('dotenv').config(); // read the .env file
-const express = require('express');
-const {getRates} = require('./lib/fixer-service');
+require("dotenv").config(); // read the .env file
+const express = require("express");
+const { getRates } = require("./lib/fixer-service");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Set public folder as root
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // Allow front-end access to node_modules folder
-app.use('/scripts', express.static(`${__dirname}/node_modules/`));
+app.use("/scripts", express.static(`${__dirname}/node_modules/`));
 
-// Redirect all traffic to index.html
-app.use((req, res) => 
-    res.sendFile(`${__dirname}/public/index.html`)
-);
+// ==================================================================== //
+// ==================  Express Error Handler ========================== //
+// ==================================================================== //
+const errorHandler = (err, req, res) => {
+  if (err.response) {
+    // The Request was made and the server responded with a status code
+    // that fails out of the range 2xx
+    res
+      .status(403)
+      .send({ title: "Server responded with an error", message: err.message });
+  } else if (err.request) {
+    // The request was made but no response was received
+    res.status(503).send({
+      title: "Unable to communicate with server",
+      message: err.message
+    });
+  } else {
+    // Something happened in setting up the request that triggered an Error
+    res
+      .status(500)
+      .send({ title: "An unexpected error occurred", message: err.message });
+  }
+};
+// ==================================================================== //
+// ================= End Express Error Handler ======================== //
+// ==================================================================== //
+
+// ==================================================================== //
+// ================ Fetch Latest Currency Rates ======================= //
+// ==================================================================== //
+/* jshint ignore:start */
+app.get("/api/rates", async (req, res) => {
+  try {
+    const data = await getRates();
+    res.setHeader("Contetnt-Type", "application/json");
+    res.send(data);
+  } catch (error) {
+    errorHandler(error, req, res);
+  }
+});
+/* jshint ignore:end */
+// ==================================================================== //
+// ============== End .get fetch Express Route ======================== //
+// ==================================================================== //
+
+// ==================================================================== //
+// ===========  Redirect all traffic to index.html =================== //
+app.use((req, res) => res.sendFile(`${__dirname}/public/index.html`));
 
 // Listen for HTTP requests on port 3000
 app.listen(port, () => {
-    console.log('listening on %d', port);
+  console.log("listening on %d", port);
 });
 
 /* jshint ignore:start */
